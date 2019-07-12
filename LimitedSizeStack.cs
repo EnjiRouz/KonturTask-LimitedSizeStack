2b7c236b3a297c1d040f73814b8ac812b209e832
Enji Rouz <enji.rouz@yandex.ru>
2019-07-12 19:06:48 +0500

    Initial commit

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        readonly LinkedList<T> list = new LinkedList<T>();
+        private readonly int limit;
+        
+        public LimitedSizeStack(int limit)
+        {
+            this.limit = limit;
+            Count = 0;
+        }
+
+        public void Push(T item)
+        {
+            if (list.Count == limit)
+            {
+                list.RemoveFirst();
+                Count--;
+            }
+
+            list.AddLast(item);
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0) throw new InvalidOperationException();
+            var result = list.Last.Value;
+            list.RemoveLast();
+            Count--;
+            return result;
+        }
+
+        public int Count { get; private set; }
+    }
+}
---
+++ ListModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class ListModel<TItem>
+    {
+        public enum ActionType
+        {
+            AddItem,
+            RemoveItem
+        }
+        
+        LimitedSizeStack<Tuple<ActionType, TItem, int>> actionStory;
+        
+        public List<TItem> Items { get; }
+        private int limit;       
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            this.limit = limit;
+            actionStory = new LimitedSizeStack<Tuple<ActionType, TItem, int>>(limit);
+        }
+
+        public void AddItem(TItem item)
+        {
+            actionStory.Push(Tuple.Create(ActionType.AddItem, item, Items.Count));
+            Items.Add(item);
+        }
+
+        public void RemoveItem(int index)
+        {
+            actionStory.Push(Tuple.Create(ActionType.RemoveItem,Items[index],index));
+            Items.RemoveAt(index);
+        }
+
+        public bool CanUndo()
+        {
+            return actionStory.Count > 0;
+        }
+
+        public void Undo()
+        {
+            var lastActoin = actionStory.Pop();
+            switch (lastActoin.Item1)
+            {
+                case ActionType.AddItem:
+                    Items.RemoveAt(actionStory.Count);
+                    break;
+                case ActionType.RemoveItem:
+                    Items.Insert(lastActoin.Item3 , lastActoin.Item2);
+                    break;
+            }
+        }
+    }
+}
